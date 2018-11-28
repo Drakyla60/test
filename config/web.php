dee468a4777b1eff5ed87809ac474141dbf9c9ce
Drakyla60 <Drakyla60@gmail.com>
2018-11-28 21:17:27 +0200

    create config app

---
+++ config/web.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roma Volkov
+ * E-mail: Drakyla60@gmail.com
+ * Date: 28.11.2018
+ * Time: 17:57
+ */
+
+return [
+    'id' => 'test',
+    'basePath' => realpath(__DIR__ . '/../')
+];
---
+++ web/index.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roma Volkov
+ * E-mail: Drakyla60@gmail.com
+ * Date: 28.11.2018
+ * Time: 17:48
+ */
+
+    require __DIR__ . "/../vendor/yiisoft/yii2/Yii.php";
+    $config = require __DIR__ . "/../config/web.php";
+    (new yii\web\Application($config))->run();
