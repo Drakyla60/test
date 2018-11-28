8c3e969ebe0bf5ca4f2ce81b55e2c61ae01beb72
Drakyla60 <Drakyla60@gmail.com>
2018-11-28 21:54:12 +0200

    add SiteController

---
+++ controllers/SiteController.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roma Volkov
+ * E-mail: Drakyla60@gmail.com
+ * Date: 28.11.2018
+ * Time: 21:50
+ */
+
+namespace app\controllers;
+
+use yii\web\Controller;
+
+class SiteController extends Controller
+{
+    public function actionIndex()
+    {
+        return 'Hello World';
+    }
+}
