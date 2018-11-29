ceed16fa2f11d4410e5113a6070b916496f58c8c
Drakyla60 <Drakyla60@gmail.com>
2018-11-29 21:14:28 +0200

    describe join and login page

--- views/user/join.php
+++ views/user/join.php
@@ -1,1 +1,12 @@
-<h1>Join Us</h1>
+<div class="panel panel-warning">
+    <div class="panel-heading">
+        <h3>Join Us</h3>
+    </div>
+    <div class="panel-body">
+        name <br>
+        email <br>
+        password <br>
+        pasword2 <br>
+        [Create]
+    </div>
+</div>
--- views/user/login.php
+++ views/user/login.php
@@ -1,1 +1,10 @@
-<h1>Login</h1>
+<div class="panel panel-info">
+    <div class="panel-heading">
+        <h3>Login</h3>
+    </div>
+    <div class="panel-body">
+        email <br>
+        password <br>
+        [Create]
+    </div>
+</div>
