96dc331270ff3caa4164609185bbaedca2400427
Drakyla60 <Drakyla60@gmail.com>
2018-12-01 23:23:25 +0200

    add functional test

---
+++ tests/acceptance/_bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roma Volkov
+ * E-mail: Drakyla60@gmail.com
+ * Date: 01.12.2018
+ * Time: 23:13
+ */
---
+++ tests/functional/_bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Roma Volkov
+ * E-mail: Drakyla60@gmail.com
+ * Date: 01.12.2018
+ * Time: 23:13
+ */
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
